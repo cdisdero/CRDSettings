e55ddec79f00d87dfb125857a650a58830386bd8
Christopher Disdero <info@code.chrisdisdero.com>
2018-03-05 18:21:07 -0800

    Initial Commit

---
+++ CRDSettings/CRDSettings.h
@@ -0,0 +1,19 @@
+//
+//  CRDSettings.h
+//  CRDSettings
+//
+//  Created by Christopher Disdero on 3/5/18.
+//  Copyright © 2018 Christopher Disdero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRDSettings.
+FOUNDATION_EXPORT double CRDSettingsVersionNumber;
+
+//! Project version string for CRDSettings.
+FOUNDATION_EXPORT const unsigned char CRDSettingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRDSettings/PublicHeader.h>
+
+
